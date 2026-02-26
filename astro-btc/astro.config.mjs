655f2f5111de3dcf29bb4faa1e61e5fbc6d1ac51
Tarquitet <pintoarquitecto@gmail.com>
2026-02-26 10:52:44 -0500

    added seo

--- astro-btc/astro.config.mjs
+++ astro-btc/astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://builtechraft.tarquitet.com',
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   image: {
     domains: ['cdn.modrinth.com', 'avatars.githubusercontent.com'],
   },
