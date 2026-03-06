1588fa72b6987d1e1cd8e18f9dbc9da434720ce5
Tarquitet <pintoarquitecto@gmail.com>
2026-03-06 15:24:26 -0500

    back into old ips again

--- astro-btc/src/config-ip.js
+++ astro-btc/src/config-ip.js
@@ -1,7 +1,7 @@
 // src/config-ip.js
 export const SERVER_INFO = {
-  javaIP: 'btc.tarquitet.com',
-  bedrockIP: 'btcbed.tarquitet.com',
+  javaIP: 'paper-fill.gl.joinmc.link',
+  bedrockIP: 'ring-untidy.gl.at.ply.gg',
   bedrockPort: '20367',
   versions: {
     minJava: '1.20.5',
