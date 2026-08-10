3a6af4b3191c3a45a96a0f6fdd50e82046f90c6e
Tarquitet <pintoarquitecto@gmail.com>
2026-08-09 20:18:04 -0500

    feat: configurar github actions para rama dist-build

--- astro-btc/astro.config.mjs
+++ astro-btc/astro.config.mjs
@@ -1,18 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://builtechraft.tarquitet.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react(), sitemap()],
-  image: {
-    domains: ['cdn.modrinth.com', 'avatars.githubusercontent.com'],
-  },
+  integrations: [sitemap()],
 });
