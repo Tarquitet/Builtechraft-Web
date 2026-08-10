e5945c62a6e7ad8220f204e089f6958da000c02c
Tarquitet <pintoarquitecto@gmail.com>
2026-08-09 20:18:40 -0500

    using bun instead of nodejs & better arquitecture web

--- _legacy/Old-src/js/server-live.js
+++
@@ -1,8 +0,0 @@
-// Obtener el año actual
-var ActualYear = new Date().getFullYear();
-
-// Construir el texto
-var texto = "© " + ActualYear + " Builtechraft, a Minecraft server.";
-
-// Actualizar el texto en el div
-document.getElementById("ServerLive").textContent = texto;
