5e80638696c24ab6d24d5a35977795dec84d3a0f
Tarquitet <pintoarquitecto@gmail.com>
2026-08-24 16:36:01 -0500

    fix #57

--- astro-btc/astro.config.mjs
+++ astro-btc/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,11 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
 
   integrations: [sitemap(), mdx()],
